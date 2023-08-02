f63cafcc0142f3b67a7923a9cbbdf9f3e3a69576
David Waltenburg <david@waltenburg.dk>
2023-08-02 12:51:41 +0200

    Opgave 5

--- TodoList.py
+++ TodoList.py
@@ -1,6 +1,8 @@
 class TodoList:
     def __init__(self, list):
+        self.MAXLISTSIZE = 10
         self.list = []
 
     def Add(self,x):
-        self.list.append(x)
+        if(len(self.list) < self.MAXLISTSIZE):
+            self.list.append(x)
