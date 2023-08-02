a575ca7d93070e2b4a613a6b51b08d46c0fca97c
David Waltenburg <david@waltenburg.dk>
2023-08-02 12:35:50 +0200

    Opgave 2

---
+++ main.py
@@ -0,0 +1,17 @@
+arr = [1,2,3,4,5,6,7,8,9,10]
+res = 0
+for item in arr:
+    if(item % 2):
+        res +=item
+print(res)
+
+"""
+res = 0
+i=0
+while(i< len(arr)):
+    res+=arr[i]
+    i+=1
+
+print(res)
+
+"""
