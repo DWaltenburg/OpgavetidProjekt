71adfbe957b065a7892bd8956db9c77076bf0ba3
David Waltenburg <david@waltenburg.dk>
2023-08-02 13:11:38 +0200

    Opgave 6

--- TodoList.py
+++ TodoList.py
@@ -1,8 +1,23 @@
 class TodoList:
-    def __init__(self, list):
+    def __init__(self):
         self.MAXLISTSIZE = 10
         self.list = []
 
-    def Add(self,x):
+    def Add(self, stringToAdd):
         if(len(self.list) < self.MAXLISTSIZE):
-            self.list.append(x)
+            self.list.append(stringToAdd)
+
+    def Remove(self, stringToRemove):
+        newList = []
+        for item in self.list:
+            if(item != stringToRemove):
+                newList.append(item)
+        self.list = newList
+
+myListObject = TodoList()
+
+myListObject.Add("test")
+myListObject.Add("testsdfgsdfg")
+
+myListObject.Remove("test")
+print(myListObject.list)
